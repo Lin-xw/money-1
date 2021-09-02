a46a72b92127134f99cdf067ba63e603310f0f85
wayy <948840255@qq.com>
2021-09-02 10:46:34 +0800

    完善收入页面

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -6,10 +6,10 @@
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
-                @update:value="onUpdateNotes"
+                :value.sync="record.notes"
       />
     </div>
-    <Tags/>
+    <Tags @update:value="record.tags = $event"/>
   </Layout>
 </template>
 
@@ -45,9 +45,14 @@
     }
 
     saveRecord() {
+      if(!this.record.tags ||this.record.tags.length === 0){
+        window.alert('请至少选择一个标签')
+        return
+      }
       this.$store.commit('createRecord', this.record);
       if (this.$store.state.createRecordError === null){
         window.alert('已保存');
+        this.record.notes = '';
       }
     }
   }
